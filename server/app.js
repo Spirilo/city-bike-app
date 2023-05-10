2c3c578db5dba432537c392aba7a307374138a4b
Niko <rappe.niko@gmail.com>
2023-05-10 10:01:19 +0300

    init connection to MongoDB

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => console.log('Connection OK'))
+  .catch((error) => console.log('Erro connecting to MongoDB', error.message))
+
+app.use(express.json())
+
+module.exports = app
